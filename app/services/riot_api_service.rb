8ac999da1f586853c119da5fbab2e1ad184ba4b6
Obeahs <JohansRMendoza@gmail.com>
2024-10-25 16:53:03 -0500

    additional api fixes

--- app/services/riot_api_service.rb
+++ app/services/riot_api_service.rb
@@ -1,25 +1,41 @@
-# app/services/riot_api_service.rb
 require 'net/http'
 require 'json'
 
 class RiotApiService
-    RIOT_API_BASE_URL = 'https://americas.api.riotgames.com'
-    SUMMONER_API_BASE_URL = 'https://na1.api.riotgames.com'
-  
-    def self.get_account_by_riot_id(game_name, tag_line)
-      url = URI("#{RIOT_API_BASE_URL}/riot/account/v1/accounts/by-riot-id/#{game_name}/#{tag_line}?api_key=#{ENV['RIOT_API_KEY']}")
-      response = Net::HTTP.get(url)
-      JSON.parse(response)
-    rescue StandardError => e
-      puts "Error: #{e.message}"
-    end
-  
-    def self.get_summoner_by_puuid(puuid)
-      url = URI("#{SUMMONER_API_BASE_URL}/lol/summoner/v4/summoners/by-puuid/#{puuid}?api_key=#{ENV['RIOT_API_KEY']}")
-      response = Net::HTTP.get(url)
-      JSON.parse(response)
-    rescue StandardError => e
-      puts "Error: #{e.message}"
-    end
+  RIOT_API_BASE_URL = 'https://americas.api.riotgames.com'
+  SUMMONER_API_BASE_URL = 'https://na1.api.riotgames.com'
+  user = 'Frogen'
+  tagLine = 'NA1'
+
+  def self.get_account_by_riot_id(game_name, tag_line)
+    url = URI("#{RIOT_API_BASE_URL}/riot/account/v1/accounts/by-riot-id/#{game_name}/#{tag_line}?api_key=#{ENV['RIOT_API_KEY']}")
+    response = Net::HTTP.get(url)
+    JSON.parse(response)
+  rescue StandardError => e
+    puts "Error: #{e.message}"
   end
-  
+
+  def self.get_summoner_by_puuid(puuid)
+    url = URI("#{SUMMONER_API_BASE_URL}/lol/summoner/v4/summoners/by-puuid/#{puuid}?api_key=#{ENV['RIOT_API_KEY']}")
+    response = Net::HTTP.get(url)
+    JSON.parse(response)
+  rescue StandardError => e
+    puts "Error: #{e.message}"
+  end
+
+  def self.get_matchlist_by_puuid(puuid)
+    url = URI("#{RIOT_API_BASE_URL}/tft/match/v1/matches/by-puuid/#{puuid}/ids?api_key=#{RIOT_API_KEY}")
+    response = Net::HTTP.get(url)
+    JSON.parse(response)
+  rescue StandardError => e
+    puts "Error: #{e.message}"
+  end
+
+  def self.get_match_data(match_id)
+    url = URI("#{SUMMONER_API_BASE_URL}/tft/match/v1/matches/#{match_id}?api_key=#{ENV['RIOT_API_KEY']}")
+    response = Net::HTTP.get(url)
+    JSON.parse(response)
+  rescue StandardError => e
+    puts "Error: #{e.message}"
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,58 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+# db/seeds.rb
+
+# Fetch account using Riot ID
+account_data = RiotApiService.get_account_by_riot_id('your_game_name', 'NA1')
+if account_data['status'].nil?
+  puuid = account_data['puuid']
+
+  # Create Users
+  user = User.create!(
+    name: account_data['gameName'],
+    riot_id: puuid
+  )
+
+  # Fetch match IDs for the user
+  matches_data = RiotApiService.get_match_data('your_match_id') # Replace 'your_match_id' with actual match ID
+
+  # Create Matches and associated data
+  matches_data['matches'].each do |match|
+    new_match = Match.create!(
+      match_id: match['metadata']['match_id'],
+      user: user,
+      game_type: match['info']['game_type']
+    )
+
+    match['info']['participants'].each do |participant|
+      new_participant = Participant.create!(
+        match: new_match,
+        participant_id: participant['puuid'],
+        placement: participant['placement']
+      )
+
+      participant['units'].each do |unit|
+        Unit.create!(
+          participant: new_participant,
+          character_id: unit['character_id'],
+          tier: unit['tier'],
+          items: unit['items'].join(',')
+        )
+      end
+    end
+
+    UserMatch.create!(user: user, match: new_match)
+
+    # Assuming you have some logic to assign champions to matches
+    MatchChampion.create!(
+      match: new_match,
+      champion_id: Faker::Number.between(from: 1, to: 10)
+    )
+  end
+
+  puts "Seed data created successfully!"
+else
+  puts "Error: #{account_data['status']['message']}"
+end
